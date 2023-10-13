a686c9c70a6635ddec8ae6797249298825496b42
Hugo Vega <61809602+hvega6@users.noreply.github.com>
2023-10-13 10:30:50 -0700

    created a form and tested the form for certain inputs

--- test/src/App.test.js
+++ test/src/App.test.js
@@ -1,4 +1,4 @@
-import { render, screen } from '@testing-library/react';
+import { fireEvent, render, screen } from '@testing-library/react';
 import App from './App';
 
 test('renders Susies Salad wabpage', () => {
@@ -6,3 +6,19 @@ test('renders Susies Salad wabpage', () => {
   const linkElement = screen.getByText("Susies' Salad S-bar");
   expect(linkElement).toBeInTheDocument();
 });
+
+scribe("Feedback Form", () => {
+  test("Submission is disabled if score is lower than 5 and there is no feedback", () => {
+    const handleSubmit = jest.fn();
+    render(<FeedbackForm onSubmit={handleSubmit}/>);
+
+    const rangeInput = screen.getByLabelText(/Score:/);
+    fireEvent.change(rangeInput, {target: {value: '4' } });
+
+    const submitButton = screen.getByRole('button');
+    fireEvent.click(submitButton);
+
+    expect(handleSubmit).not.toHaveBeenCalled();
+    expect(submitButton),toHaveAttribute('disabled');
+  })
+})
---
+++ test/src/FeedbackForm.js
@@ -0,0 +1,49 @@
+import { states, useState } from 'react';
+
+function FeedbackForm({onSubmit}) {
+    const [score, setScore] = useState("10");
+    const [comment, setComment] = useState("");
+
+    const isDisabled = Number(score) < 5 && comment.length <= 10;
+
+    const textAreaPlaceholder = isDisabled
+        ? "Please provide a comment explaining why the experience was not good. Minimum length is 10 characters"
+        : "Optinal Feedback"
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        onSubmit(score, comment);
+    };
+
+    return(
+        <div className='App'>
+            <form onSubmit={handleSubmit}>
+                <fieldset>
+                    <h2>Feedback form</h2>
+                    <div className='field'>
+                        <label>Score: {score} ⭐</label>
+                        <input
+                            value={score}
+                            onChange={(e) => {
+                                setScore(e.target.value);
+                            }}
+                            type='range'
+                            min='0'
+                            max='10'
+                        />
+                    </div>
+                    <div className='field'>
+                        <label>Comments:</label>
+                        <textarea
+                          placeholder={textAreaPlaceholder}
+                          name='comment'
+                          onChange={(e) => {
+                            setComment(e.target.value);
+                          }}
+                        />
+                    </div>
+                </fieldset>
+            </form>
+        </div>
+    )
+}
